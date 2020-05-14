8f328bb931d24e354572d9bf9421007324988192
Patrick Miller <pmiller1208@gmail.com>
2020-05-14 14:56:30 -0400

    finished two challenges

--- index.js
+++ index.js
@@ -36,9 +36,12 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return ('Goodbye, ' + name + '. Have a great day')
 }
+ 
+sayGoodbye('Patrick');
+console.log(sayGoodbye('Patrick'));
 
 /**
  * ### Challenge `temperatureCtoF`
@@ -54,9 +57,19 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
-  /* code here */
+// let temperatureF = (celcius * (9 / 5)) + 32;
+
+function temperatureCtoF(celcius) {
+  return Math.round((celcius * (9/5) + 32))
+
+  
 }
+
+temperatureCtoF();
+  console.log(temperatureCtoF(24));
+
+
+
 
 /**
  * ### Challenge `temperatureInF`
